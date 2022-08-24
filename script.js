6684645edeefc136779647f4706d99580c062783
Khaled Emad <khaledk212@gmail.com>
2022-08-24 23:39:09 +0200

    added variable rating and stars

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ fetch("http://localhost:3000/body")
   .catch((err) => console.log(err));
   
   
-let addCourses = () => {
+const addCourses = () => {
     courses_data = []
     filter = search_bar.value;
     init_data.forEach((course) => {
@@ -22,18 +22,26 @@ let addCourses = () => {
     let container = document.querySelector(".courses-container")
     container.innerHTML = ''
     courses_data.forEach((course) => {
+        let rating = course.rating.toPrecision(2);
+        let stars = "";
+        for(let count = 1; count<=5; count++){
+            if(count < rating)
+            stars += `<i class="fas fa-star" style="color: #f4c150"></i>\n`
+            else if(rating != Math.floor(rating)){
+                stars += `<i class="fas fa-star-half-alt" style="color: #f4c150"></i>\n`
+                rating = Math.floor(rating)
+            }
+            else
+                stars += `<i class="fas fa-star" style="color: lightgray"></i>\n`
+        }
         container.innerHTML += `
             <div class="img-container">
             <figure>
                 <img class="course-img" src="${course.image}" alt="Course image">
                 <figcaption class ="cap1">${course.title}</figcaption>
                 <figcaption class ="cap2">${course.instructors[0].name}</figcaption>
-                <i>4.6</i>
-                <i class="fas fa-star" style="color: #f4c150"></i>
-                <i class="fas fa-star" style="color: #f4c150"></i>
-                <i class="fas fa-star" style="color: #f4c150"></i>
-                <i class="fas fa-star" style="color: #f4c150"></i>
-                <i class="fas fa-star-half-alt" style="color: #f4c150"></i>
+                <i>${course.rating.toPrecision(2)}</i>
+                ${stars}
                 <figcaption class ="cap1">E£${course.price}</figcaption>
             </figure>
             </div>
@@ -42,7 +50,7 @@ let addCourses = () => {
     });
 };
 
-let search_bar = document.querySelector("input");
+const search_bar = document.querySelector("input");
 search_bar.addEventListener("keyup", (e) => {
     filter = e.target.value;
     addCourses();
