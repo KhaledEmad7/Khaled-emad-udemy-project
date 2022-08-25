567e3464b4bc963fb3780fbee972fab7b3efff58
Khaled Emad <khaledk212@gmail.com>
2022-08-25 03:34:52 +0200

    Made courses carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let active_course = document.querySelector(".pressed");
 let courses_data = []
 let init_data = []
 let filter = ""
@@ -5,7 +6,7 @@ let filter = ""
 fetch("http://localhost:3000/body")
   .then((res) => res.json())
   .then((data) => {
-    init_data = data.courses
+    init_data = data
     addCourses()
   })
   .catch((err) => console.log(err));
@@ -14,41 +15,92 @@ fetch("http://localhost:3000/body")
 const addCourses = () => {
     courses_data = []
     filter = search_bar.value;
-    init_data.forEach((course) => {
+    init_data[active_course.textContent].courses.forEach((course) => {
         if (course.title.toLowerCase().includes(filter.toLocaleLowerCase())) {
             courses_data.push(course);
         }
     });
-    let container = document.querySelector(".courses-container")
+    let container = document.querySelector(".carousel-inner")
     container.innerHTML = ''
-    courses_data.forEach((course) => {
-        let rating = course.rating.toPrecision(2);
-        let stars = "";
-        for(let count = 1; count<=5; count++){
-            if(count < rating)
-            stars += `<i class="fas fa-star" style="color: #f4c150"></i>\n`
-            else if(rating != Math.floor(rating)){
-                stars += `<i class="fas fa-star-half-alt" style="color: #f4c150"></i>\n`
-                rating = Math.floor(rating)
+    
+    let idx = 0;
+    for(let cur = 0; cur<courses_data.length/5; cur++){
+        let cnt = 0;
+        let add = "";
+        while(cnt<5){
+            cnt++;
+            if(idx>=courses_data.length){
+                add += `<div class="card"></div>`
+                continue;
             }
-            else
+            let course = courses_data[idx++];
+            console.log(course.title);
+            let rating = course.rating.toPrecision(2);
+            let stars = "";
+            for(let count = 1; count<=5; count++){
+                if(count < rating)
+                stars += `<i class="fas fa-star" style="color: #f4c150"></i>\n`
+                else if(rating != Math.floor(rating)){
+                    stars += `<i class="fas fa-star-half-alt" style="color: #f4c150"></i>\n`
+                    rating = Math.floor(rating)
+                }
+                else
                 stars += `<i class="fas fa-star" style="color: lightgray"></i>\n`
-        }
-        container.innerHTML += `
-            <div class="img-container">
-            <figure>
-                <img class="course-img" src="${course.image}" alt="Course image">
-                <figcaption class ="cap1">${course.title}</figcaption>
-                <figcaption class ="cap2">${course.instructors[0].name}</figcaption>
-                <i>${course.rating.toPrecision(2)}</i>
-                ${stars}
-                <figcaption class ="cap1">E£${course.price}</figcaption>
-            </figure>
+            }
+            add += `
+            <div class="card">
+                <div class="img-container">
+                    <figure>
+                        <img class="course-img" src="${course.image}" alt="Course image">
+                        <figcaption class ="cap1">${course.title}</figcaption>
+                        <figcaption class ="cap2">${course.instructors[0].name}</figcaption>
+                        <i>${course.rating.toPrecision(2)}</i>
+                        ${stars}
+                        <figcaption class ="cap1">E£${course.price}</figcaption>
+                    </figure>
+                </div>
             </div>
-        
-        `
-    });
+            `
+        }
+        if(!cur)
+            container.innerHTML +=  `
+                <div class="carousel-item active">
+                    <div class="cards-wrapper">
+                        ${add}
+                    </div>
+                </div>
+            `
+        else
+            container.innerHTML +=  `
+                <div class="carousel-item">
+                    <div class="cards-wrapper">
+                        ${add}
+                    </div>  
+                </div>
+            `
+    }
 };
+
+let clicked = document.querySelector(".courses-categories").children;
+
+let onChangeCourse = (event) => {
+    course = event.target;
+    if (course != active_course) {
+      active_course.classList.toggle("pressed");
+      active_course.classList.toggle("norm");
+      course.classList.toggle("pressed");
+      course.classList.toggle("norm");
+      active_course = course;
+      addCourses();
+    }
+  };
+  
+  
+for(let event of clicked) {
+    event.addEventListener("click", onChangeCourse);
+}
+  
+
 
 const search_bar = document.querySelector("input");
 search_bar.addEventListener("keyup", (e) => {
